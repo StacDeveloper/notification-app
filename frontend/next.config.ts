daf64824d4ea897dbd7c04265424bacc06ed6cec
Soham Mule <sohammule09@gmail.com>
2026-07-04 17:16:29 +0100

    added-api

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
-      { source: "/api/:path*", destination: "https://notification-app-gzic.vercel.app/:path*" }
+      { source: "/api/:path*", destination: "https://notification-app-gzic.vercel.app/api/:path*" }
     ]
   },
   /* config options here */
