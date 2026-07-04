6d93bf2eb9eda10c9ff6d7779b15d48c135bea3f
Soham Mule <sohammule09@gmail.com>
2026-07-04 17:02:35 +0100

    change-config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      { source: "/api/:path", destination: "https://notification-app-gzic.vercel.app/:path" }
+    ]
+  },
   /* config options here */
   reactCompiler: true,
 };
